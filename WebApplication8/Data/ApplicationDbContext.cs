e760e162ad26c26e58bc228603fd141e7f114a97
User <User@DESKTOP-VK8F4PT>
2024-12-16 18:01:42 +0200

    Add project files.

---
+++ WebApplication8/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication8.Models.Entities;
+
+namespace WebApplication8.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+       
+        {  
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
